262cd99b551c459ffa4b69093d65205eb45de2dc
csergey8 <c.sergey@protonmail.com>
2019-04-05 16:22:18 +0300

    menu was finished

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue';
-import { MdButton, MdDrawer } from 'vue-material/dist/components'; 
+import VueResource from 'vue-resource';
+import { MdButton, MdDrawer, MdCard } from 'vue-material/dist/components'; 
 import 'vue-material/dist/vue-material.min.css';
 
 
 //Vue.config.productionTip = false;
-
+Vue.use(VueResource);
 Vue.use(MdButton);
 Vue.use(MdDrawer);
+Vue.use(MdCard);
+
+Vue.http.options.root = 'https://domnoo-9744c.firebaseio.com/';
 
 new Vue({
   render: h => h(App),
