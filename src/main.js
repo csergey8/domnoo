f2ae37773dee5e038b7950794a6eeea00d6c8ad0
csergey8 <c.sergey@protonmail.com>
2019-04-06 13:02:21 +0300

    scroll reveal animation on menu

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import VueResource from 'vue-resource';
 import { MdButton, MdDrawer, MdCard } from 'vue-material/dist/components'; 
 import 'vue-material/dist/vue-material.min.css';
+import VueScrollReveal from 'vue-scroll-reveal';
 
 
 //Vue.config.productionTip = false;
@@ -10,6 +11,11 @@ Vue.use(VueResource);
 Vue.use(MdButton);
 Vue.use(MdDrawer);
 Vue.use(MdCard);
+Vue.use(VueScrollReveal, {
+  duration: 800,
+  scale: 1,
+  distance: '50px'
+});
 
 Vue.http.options.root = 'https://domnoo-9744c.firebaseio.com/';
 
